175b5e19289bbd5d01599bbac1b1f666ea95f838
Josh Cooke <josh.cooke@starlingbank.com>
2020-10-21 14:49:36 +0100

    add basic pojo

---
+++ src/main/java/avro/proto/BasicPojo.java
@@ -0,0 +1,35 @@
+package avro.proto;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+
+public class BasicPojo {
+
+  private final long id;
+  private final String name;
+  private final ZonedDateTime timestamp;
+  private final BigDecimal[] rates;
+
+  public BasicPojo(long id, String name, ZonedDateTime timestamp, BigDecimal[] rates) {
+    this.id = id;
+    this.name = name;
+    this.timestamp = timestamp;
+    this.rates = rates;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public ZonedDateTime getTimestamp() {
+    return timestamp;
+  }
+
+  public BigDecimal[] getRates() {
+    return rates;
+  }
+}
